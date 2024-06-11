53fac2f66e01a58b42de430cb9b960cac8145849
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-11 12:37:37 -0400

    feat: add example file

---
+++ example/__init__.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from lomography.base import Lomography
+from lomography.utils.requests import get
+from os import environ
+
+load_dotenv()
+lomo = Lomography(api_key=environ["LOMOGRAPHY_API_KEY"])
+get(lomo, "/photos/recent")
