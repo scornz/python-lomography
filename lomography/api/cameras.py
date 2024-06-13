7097ed82f3cc89d5d7e0e89123dcf8cbc477352d
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-13 12:18:42 -0400

    feat: add typed functions for /cameras

--- lomography/api/cameras.py
+++ lomography/api/cameras.py
@@ -2,7 +2,52 @@
 from lomography.base import Lomography
 
 # Typing
-from .types import PhotosResponseDict
+from .types import CameraDict, PhotosResponseDict
 
 # Utilities
 from lomography.utils.requests import get
+
+
+def fetch_camera_by_id(lomo: Lomography, camera_id: int) -> CameraDict:
+    """Fetch a singular camera by its unique ID. This will return a single camera.
+
+    Args:
+        `lomo` (Lomography): An instance of the Lomography class.
+        `camera_id` (int): The unique ID of the camera.
+
+    Returns:
+        CameraDict: A dictionary containing the camera data.
+    """
+    return get(lomo, f"/cameras/{camera_id}")
+
+
+def fetch_popular_photos_by_camera_id(
+    lomo: Lomography, camera_id: int
+) -> PhotosResponseDict:
+    """Fetch popular photos from a specific camera. This will return the most
+    popular photos (uploaded in the last month) taken with that camera.
+
+    Args:
+        `lomo` (Lomography): An instance of the Lomography class.
+        `camera_id` (int): The unique ID of the camera.
+
+    Returns:
+        PhotosResponseDict: A dictionary containing the metadata and a list of photos.
+    """
+    return get(lomo, f"/cameras/{camera_id}/photos/popular")
+
+
+def fetch_recent_photos_by_camera_id(
+    lomo: Lomography, camera_id: int
+) -> PhotosResponseDict:
+    """Fetch recent photos from a specific camera. This will return the most
+    recent photos (right as they are uploaded).
+
+    Args:
+        `lomo` (Lomography): An instance of the Lomography class.
+        `camera_id` (int): The unique ID of the camera.
+
+    Returns:
+        PhotosResponseDict: A dictionary containing the metadata and a list of photos.
+    """
+    return get(lomo, f"/cameras/{camera_id}/photos/recent")
