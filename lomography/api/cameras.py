044fe051dc196cb6d3d066ad2b29716ff8002e25
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-13 12:22:43 -0400

    feat: add fetch_cameras function

--- lomography/api/cameras.py
+++ lomography/api/cameras.py
@@ -2,10 +2,30 @@
 from lomography.base import Lomography
 
 # Typing
-from .types import CameraDict, PhotosResponseDict
+from typing import List, TypedDict
+from .types import CameraDict, MetaDict, PhotosResponseDict
 
 # Utilities
 from lomography.utils.requests import get
+
+
+class CamerasResponseDict(TypedDict):
+    """A dictionary representing a response of cameras."""
+
+    meta: MetaDict
+    cameras: List[CameraDict]
+
+
+def fetch_cameras(lomo: Lomography) -> CamerasResponseDict:
+    """Fetch all cameras. This will return a list of all cameras.
+
+    Args:
+        `lomo` (Lomography): An instance of the Lomography class.
+
+    Returns:
+        CamerasResponseDict: A dictionary containing the metadata and a list of cameras.
+    """
+    return get(lomo, "/cameras")
 
 
 def fetch_camera_by_id(lomo: Lomography, camera_id: int) -> CameraDict:
