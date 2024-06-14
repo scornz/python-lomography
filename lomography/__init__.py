40d10aa6633c97a18ab81ee6ccb97f12e7c4ce88
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-14 14:55:12 -0400

    chore: add objects to base __init__ file

--- lomography/__init__.py
+++ lomography/__init__.py
@@ -1,4 +1,12 @@
 # Export necessary classes to be exposed to the user
 from .base import Lomography
+from .objects import (
+    LomoCamera,
+    LomoFilm,
+    LomoPhoto,
+    LomoUser,
+    LomoPhotoImage,
+    LomoImage,
+)
 
 __version__ = "0.1.0"
