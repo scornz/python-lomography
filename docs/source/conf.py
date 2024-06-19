71a9c4cd4e193e2a1a0186a6d3ee78ed4beb52b2
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-19 13:46:26 -0400

    fix: add aiohttp as requirement for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,8 @@
 import os
 import sys
 
+# -- Path setup
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 # -- Project information
 
