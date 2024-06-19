67164cde0254fc13fba70b6a15ecdbded50931db
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-19 13:38:39 -0400

    fix: use absolute path in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../lomography"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 # -- Project information
 
