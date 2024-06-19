cace1e5fb6e6b845e3af5017f09b75344a629af6
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-06-18 20:34:17 -0400

    chore: delete unnecessary example folder

--- example/__init__.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from lomography.base import Lomography
-from lomography.utils.requests import get
-from os import environ
-
-load_dotenv()
-lomo = Lomography(api_key=environ["LOMOGRAPHY_API_KEY"])
-lomo.close()
